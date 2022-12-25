9b3d7d05625419d1f2d338d0641b57ff4a95d2cc
xuyb1999 <xuyibin17@mails.ucas.ac.cn>
2022-12-25 16:38:17 +0800

    Delete rebuild-qemu.sh and run-qemu.sh, and put their content into README of DASICS project

--- rebuild-qemu.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-NUM_PROCESSOR=`cat /proc/cpuinfo | grep processor | wc -l`
-echo "Processors: ${NUM_PROCESSOR}"
-./configure  --target-list=riscv64-softmmu
-make clean && make -j${NUM_PROCESSOR}
--- run_qemu.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-sudo /home/ws/qemu/build-dasics/riscv64-softmmu/qemu-system-riscv64 -M virt -m 1G \
-	-nographic -kernel riscv-pk/build/bbl \
-	-append "console=ttyS0 rw root=/dev/vda" \
-	-drive file=img,format=raw,id=hd0 \
-	-device virtio-blk-device,drive=hd0 \
-	-bios none  -s -S
